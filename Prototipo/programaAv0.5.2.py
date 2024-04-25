c846c6442bb4d9a4d8fd5ae2b93b3dc3dd3e8ddb
Brian Alarcon Vergara <83990185+brianEsteban0@users.noreply.github.com>
2024-04-25 12:07:23 -0400

    actualizacion prototipe

--- Prototipo/programaAv0.5.2.py
+++ Prototipo/programaAv0.5.2.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import math
+#import date
 # Función para dibujar puntos en los vértices de los contornos
 def dibujar_puntos(image, contours):
     for contour in contours:
@@ -18,6 +19,8 @@ k = 0.04
 savedPoints = []
 savedPointsReserve = []
 copySavedPoints = []
+distancias = []
+liness = 0
 
 def calcular_distancia(punto1, punto2):
         x1, y1 = punto1
@@ -34,10 +37,33 @@ def dibujando(event, x, y, flags, param):
                     savedPointsReserve.append(esquina)
                     break
 
+def dibujarLineas(image_copy3,copySavedPoint):
+    perimetro = 0
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    if len(copySavedPoint) == 2:
+        distancia = calcular_distancia(copySavedPoint[0], copySavedPoint[1])
+        perimetro = perimetro + distancia  
+        cv2.line(image_copy3,copySavedPoint[0],copySavedPoint[1],(0,255,255),2)
+        cv2.putText(image_copy3,"{:.3f}".format(distancia),copySavedPoint[0],font,1,(0,0,255),2,cv2.LINE_AA)
+        if len(distancias) <= 3:
+            distancias.append((copySavedPoint[0],copySavedPoint[1]))
+def dibujarLineasciclo(image_copy3, dist):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    for line in dist:
+        punto1,punto2,diss = line 
+        cv2.line(image_copy3,punto1,punto2,(0,255,255),2)
+        cv2.putText(image_copy3,"{:.3f}".format(diss),punto1,font,1,(0,0,255),2,cv2.LINE_AA)
+def lineas4(event, x, y, flags, param):
+    if liness < 4:
+        dibujarLineas(event, x, y, flags, param)
+        i+=1
+
+
+
 def dibujar_circulos(pintados,img):
     for circle in pintados:
         cv2.circle(img, circle, 5, (0, 0, 255), -1)
-     
+
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
@@ -56,7 +82,7 @@ while cap.isOpened():
         cv2.circle(frame, (x, y), 3, (0, 255, 0), -1)
 
     cv2.namedWindow('Shi-tomasi Detecction')
-    cv2.setMouseCallback('Shi-tomasi Detecction',dibujando)
+    cv2.setMouseCallback('Shi-tomasi Detecction',lineas4)
     dibujar_circulos(savedPointsReserve, frame)
     # Mostrar las esquinas detectadas
     cv2.imshow('Shi-tomasi Detecction', frame)
